6ac64b14c8eb71e1f37d5d3e3b913a692369f3f3
guylhermee <guylhermee.b@hotmail.com>
2023-01-17 23:21:34 -0300

    Basic application of Vite and initial testing based on the playwright's documentation

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Testes</h1>
+      <form action="https://demo.playwright.dev/todomvc">
+        <input type="submit" value="TODO List" />
+      </form>
+      <p className="access-link">
+        Clique no link para visitar a demo de uma ToDO List criada pelo PlayWright e confira o que está sendo testado!
+      </p>
+    </div>
+  )
+}
+
+export default App
